856a6eb4f57cdfa54b027cd3168f0843081a312a
Jeff Eboreime <1802329@laurea.fi>
2020-09-01 20:41:29 +0300

    1.2 completed

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,14 +10,6 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
-  return (
- <>
-  <p>{props.part} {props.count}</p>
- </>  
-  )
-}
-
 const Total = (props) => {
   return (
     <>
@@ -28,22 +20,46 @@ const Total = (props) => {
   )
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
+const Content = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  return (
+ <>
+  <Part msg={part1} nro={exercises1} />
+  <Part msg={part2} nro={exercises2} />
+  <Part msg={part3} nro={exercises3} />
+ </>  
+  )
+}
+
+const Part = (props) => {
+  
+  return (
+    <>
+    <p>{props.msg} {props.nro}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+ // const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+ // const part2 = 'Using props to pass data'
+  const exercises2 = 7
+ // const part3 = 'State of a component'
+  const exercises3 = 14
 
   
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} count={exercises1}/>
-      <Content part={part2} count={exercises2}/>
-      <Content part={part3} count={exercises3}/>
+      <Content/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
