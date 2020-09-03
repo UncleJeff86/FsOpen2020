63ba59953e566e9ed294caca63dcbd78f4864376
Jeff Eboreime <1802329@laurea.fi>
2020-09-03 23:47:18 +0300

    1.7 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -11,34 +11,47 @@ const App = (props) => {
 const [feedbackCounter, setReview] = useState({
   good: 0, 
   neutral: 0, 
-  bad: 0
+  bad: 0,
+  all: 0,
+  score: 0
 })
 
+  
+
   const handleGood = () => {
+    console.log("Good review!")
     const newReview ={
       ...feedbackCounter,
       good: feedbackCounter.good + 1,
+      all: feedbackCounter.all +1,
+      score: feedbackCounter.score +1
       
     }
     setReview(newReview)
   }
 
   const handleNeutral = () => {
+    console.log("Neutral review")
     const newReview ={
       ...feedbackCounter,
       neutral: feedbackCounter.neutral + 1,
+      all: feedbackCounter.all +1
     }
     setReview(newReview)
   }
 
   const handleBad = () => {
+    console.log("Bad review!")
     const newReview ={
       ...feedbackCounter,
-      bad: feedbackCounter.bad + 1
+      bad: feedbackCounter.bad + 1,
+      all: feedbackCounter.all +1,
+      score: feedbackCounter.score -1
     }
     setReview(newReview)
   }
-
+console.log("Score: ", feedbackCounter.score)
+console.log("Count: ", feedbackCounter.all)
   return (
     <div>
       <h1>give feedback</h1>
@@ -52,10 +65,13 @@ const [feedbackCounter, setReview] = useState({
       <p>
       good {feedbackCounter.good}<br/>
       neutral {feedbackCounter.neutral}<br/>
-      bad {feedbackCounter.bad}
+      bad {feedbackCounter.bad}<br/>
+      all {feedbackCounter.all}<br/>
+      average {feedbackCounter.score / feedbackCounter.all}<br/>
+      positive {feedbackCounter.good / feedbackCounter.all * 100} %<br/>      
       </p>
            
-      </div>
+    </div>
   )
 }
 
